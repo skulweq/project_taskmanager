b3d361cf261633e7ee03ae8fdf84ab9dd8846870
skulweq <zakiremil2519@gmail.com>
2025-05-26 22:01:42 +0300

    fix mainwindow2.h

---
+++ untitled/headers/mainwindow2.h
@@ -0,0 +1,30 @@
+#ifndef MAINWINDOW2_H
+#define MAINWINDOW2_H
+
+#include <QMainWindow>
+
+class Form;
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow2;
+}
+QT_END_NAMESPACE
+
+class MainWindow2 : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow2(QWidget *parent = nullptr);
+    ~MainWindow2();
+
+private slots:
+    void on_pushButton_3_clicked();
+
+private:
+    Ui::MainWindow2 *ui;
+    Form *form;
+};
+
+#endif // MAINWINDOW2_H
