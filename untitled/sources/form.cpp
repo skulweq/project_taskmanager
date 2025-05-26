00fa34f6453622761f0c60526614b82fb8a6bbeb
skulweq <zakiremil2519@gmail.com>
2025-05-27 00:10:06 +0300

    added one more button "главная" to form.cpp

--- untitled/sources/form.cpp
+++ untitled/sources/form.cpp
@@ -1,4 +1,5 @@
 #include "form.h"
+#include "mainwindow2.h"
 #include "profile.h"
 #include "ui_form.h"
 
@@ -17,6 +18,13 @@ Form::~Form()
 void Form::on_pushButton_clicked()
 {
     profile->show();
+    this->hide();
+}
+
+
+void Form::on_pushButton_2_clicked()
+{
+    mainwindow2->show();
     this->hide();
 }
 
