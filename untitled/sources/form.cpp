c78ba6c6607e48ed3eb514a3d7de2e3cdb69c1b4
skulweq <zakiremil2519@gmail.com>
2025-05-27 16:07:57 +0300

    fixed form.cpp

--- untitled/sources/form.cpp
+++ untitled/sources/form.cpp
@@ -1,10 +1,10 @@
 #include "form.h"
-#include "mainwindow2.h"
-#include "profile.h"
 #include "ui_form.h"
+#include "profile.h"
 
 Form::Form(QWidget *parent)
-    : QWidget(parent), ui(new Ui::Form)
+    : QWidget(parent)
+    , ui(new Ui::Form)
 {
     ui->setupUi(this);
     profile = new class profile(this);  // Передаём this (Form) в profile
@@ -18,13 +18,6 @@ Form::~Form()
 void Form::on_pushButton_clicked()
 {
     profile->show();
-    this->hide();
-}
-
-
-void Form::on_pushButton_2_clicked()
-{
-    mainwindow2->show();
-    this->hide();
+    this->close();
 }
 
