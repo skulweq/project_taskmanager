84c0a30fb3761dedaffa0e73d4da6db84e1182b9
skulweq <zakiremil2519@gmail.com>
2025-06-09 13:44:54 +0300

    Update form.cpp

--- untitled/sources/form.cpp
+++ untitled/sources/form.cpp
@@ -1,23 +1,36 @@
 #include "form.h"
 #include "ui_form.h"
 #include "profile.h"
+#include "mainwindow2.h"
 
 Form::Form(QWidget *parent)
-    : QWidget(parent)
-    , ui(new Ui::Form)
+    : QWidget(parent),
+    ui(new Ui::Form),
+    mainwindow2(nullptr)
 {
     ui->setupUi(this);
-    profile = new class profile(this);  // Передаём this (Form) в profile
+    connect(ui->pushButton, &QPushButton::clicked, this, &Form::on_pushButton_clicked);
 }
 
 Form::~Form()
 {
     delete ui;
+    delete mainwindow2;
 }
 
 void Form::on_pushButton_clicked()
 {
-    profile->show();
-    this->close();
-}
+    QString title = ui->lineEdit->text();
+    QString description = ui->textEdit->toPlainText();
 
+    if (!title.isEmpty()) {
+        emit taskCreated(title, description);
+        this->close();
+    }
+
+    if (!mainwindow2) {
+        mainwindow2 = new MainWindow2(this); // Указываем родителя
+    }
+    mainwindow2->show();
+    this->hide(); // Или close(), но тогда нужно изменить логику
+}
