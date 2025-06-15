0d404f45b70c1ad15f6c3af46e9b84e58db7e04f
skulweq <zakiremil2519@gmail.com>
2025-06-15 22:39:15 +0300

    Update task.cpp

--- untitled/sources/task.cpp
+++ untitled/sources/task.cpp
@@ -8,17 +8,19 @@ QString Task::getTitle() const { return m_title; }
 QString Task::getDescription() const { return m_description; }
 
 QString Task::toString() const {
-    return QString("%1|%2|%3")
+    return QString("%1|%2|%3|%4")
     .arg(m_title)
         .arg(m_description)
-        .arg(m_completed ? "1" : "0");
+        .arg(m_completed ? "1" : "0")
+        .arg(m_dueDate.toString(Qt::ISODate));  // Формат "yyyy-MM-dd"
 }
 
 Task Task::fromString(const QString &str) {
     QStringList parts = str.split("|");
-    if (parts.size() >= 3) {
+    if (parts.size() >= 4) {
         Task task(parts[0], parts[1]);
         task.setCompleted(parts[2] == "1");
+        task.setDueDate(QDate::fromString(parts[3], Qt::ISODate));
         return task;
     }
     return Task("", "");
@@ -32,3 +34,10 @@ void Task::setCompleted(bool completed) {
     m_completed = completed;
 }
 
+QDate Task::dueDate() const {
+    return m_dueDate;
+}
+
+void Task::setDueDate(const QDate &date) {
+    m_dueDate = date;
+}
